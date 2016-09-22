1905386338fecf68c65b9de01fba61414fe7b88b
robotmayo <robotmayo@gmail.com>
2016-09-22 10:13:52 -0400

    added capture tests

---
+++ test/utils/capture.test.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+
+import capture from '../../src/utils/capture';
+
+test('capture', function(t){
+  const c = capture(() => {
+    throw new Error();
+  });
+  t.truthy(c.err);
+  const v = capture(() => 'works');
+  t.is(v.value, 'works');
+});
