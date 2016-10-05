f01e339cb50d178cc8e78baada36efb84782a306
robotmayo <robotmayo@gmail.com>
2016-10-04 20:03:14 -0400

    Started work on the userservice integration test

--- test/integration/service/user.test.js
+++ test/integration/service/user.test.js
@@ -1,10 +1,55 @@
 import test from 'ava';
 import mysql from 'mysql';
+import faker from 'faker';
+import {NotFoundError} from '../../../src/utils/cerr';
 
-const pool = mysql.createPool(require('../config.test.json'));
+const pool = mysql.createPool(require('../config.test.json').database);
 
-import UserServiceFn from '../../src/services/user';
+import UserServiceFn from '../../../src/services/user';
 
-test('getUserById', async function(t){
-  t.pass();
+function genUser(){
+  return {
+    id: -1,
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password()
+  };
+}
+
+test('getUserBy*', async function(t){
+  const fakeUser = genUser();
+  const UserService = UserServiceFn({connection: pool});
+  const id = await UserService.saveUser(
+    fakeUser.email,
+    fakeUser.username,
+    fakeUser.password
+  );
+  fakeUser.id = id;
+  const user = await UserService.getUserById(id);
+  t.deepEqual(fakeUser, user);
+  try {
+    await UserService.getUserById(-1);
+    t.fail();
+  } catch (err) {
+    if(err instanceof NotFoundError) return t.pass();
+    t.fail();
+  }
+
+  const fakeUserForEmail = genUser();
+  const id2 = UserService.saveUser(
+    fakeUserForEmail.email,
+    fakeUserForEmail.username,
+    fakeUserForEmail.password
+  );
+  fakeUserForEmail.id = id2;
+  const user2 = await UserService.getUserByEmail(fakeUserForEmail.email);
+  t.deepEqual(fakeUserForEmail, user2);
+  try {
+    await UserService.getUserByEmail('wOWOOWWOO');
+    t.fail();
+  } catch (err) {
+    if(err instanceof NotFoundError) return t.pass();
+    t.fail();
+  }
+
 });
