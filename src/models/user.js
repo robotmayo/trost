05b0e7c48352be9cd521c7457e5e52844836a151
robotmayo <robotmayo@gmail.com>
2016-10-06 22:42:25 -0400

    Get user by email or id

--- src/models/user.js
+++ src/models/user.js
@@ -18,15 +18,20 @@ const GET_BY_ID = `
   WHERE id = ?;
 `;
 
-function gotResults(msg){
-  return function(results){
-    if(results[0] == null) return Promise.reject(new NotFoundError(msg));
+const GET_BY_USERNAME_OR_EMAIL = `
+  SELECT id, email, username, password FROM trost_users
+  WHERE username = ? OR email = ?
+`;
+
+function gotResults(msg) {
+  return function (results) {
+    if (results[0] == null) return Promise.reject(new NotFoundError(msg));
     return results[0];
   }
 }
 
 module.exports = function (userOpts) {
-  const Model = Object.assign({connection: require('../db')}, userOpts);
+  const Model = Object.assign({ connection: require('../db') }, userOpts);
 
   Model.saveUser = function saveUser(email, username, password) {
     if (!email) throw new ValidationError('EMAIL REQUIRED');
@@ -41,9 +46,12 @@ module.exports = function (userOpts) {
     switch (type) {
       case 'email':
         return query(Model.connection, GET_BY_EMAIL, args)
-        .then(gotResults('USER_NOT_FOUND'));
+          .then(gotResults('USER_NOT_FOUND'));
       case 'id':
         return query(Model.connection, GET_BY_ID, args)
+          .then(gotResults('USER_NOT_FOUND'));
+      case 'uoe':
+        return query(Model.connection, GET_BY_USERNAME_OR_EMAIL, args[0], args[0])
           .then(gotResults('USER_NOT_FOUND'));
       default:
         return Promise.reject(new ValidationError(`INVALID TYPE OF ${type}`));
@@ -52,6 +60,7 @@ module.exports = function (userOpts) {
 
   Model.getUserByEmail = Model.getUserBy.bind(null, 'email');
   Model.getUserById = Model.getUserBy.bind(null, 'id');
+  Model.getUserByUsernameOrEmail = Model.getUserBy.bind(null, 'uoe');
 
   return Model;
 };
